517059895ccffbdb370bf894a4fa5c5994b53295
Ubaldo Cotta <ucotta@me.com>
2021-01-28 12:38:00 +0100

    just added project

---
+++ Shared/TestKeylessiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestKeylessiOSApp.swift
+//  Shared
+//
+//  Created by Ubaldo Cotta on 28/1/21.
+//
+
+import SwiftUI
+
+@main
+struct TestKeylessiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
